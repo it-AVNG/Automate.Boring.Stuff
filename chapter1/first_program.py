44b545c791e7a53e23962bd1b17327181515e0ba
it-AVNG <it.avng@gmail.com>
2023-01-24 07:50:57 +0700

    Write first program using input()

---
+++ chapter1/first_program.py
@@ -0,0 +1,11 @@
+#this program ask for name and reponse
+
+print('Hello, world')
+print('What is your name?')
+myName = input()
+print('it is good to meet you ' + myName)
+print('length of your name is')
+print(len(myName))
+print('How old are you')
+myAge = input()
+print('you will be '+ str(int(myAge)+1) + ' in a year')
