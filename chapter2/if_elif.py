8c830a50fd338cd0c8e9858cc798b6ea8a046d3b
it-AVNG <it.avng@gmail.com>
2023-02-01 22:18:43 +0700

    Add if statement example

---
+++ chapter2/if_elif.py
@@ -0,0 +1,12 @@
+print('enter your name: ')
+name = input()
+print('enter your age: ')
+age = input()
+
+if name == 'Alice': print('Hi, Alice')
+elif age< 12: print('you are not valid')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+
