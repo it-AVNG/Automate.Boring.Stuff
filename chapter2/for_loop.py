ae1a9b801648bcf7a08efab7e86ede09d0fd1749
it-AVNG <it.avng@gmail.com>
2023-02-09 17:26:11 +0700

    add simple example for loop

---
+++ chapter2/for_loop.py
@@ -0,0 +1,11 @@
+print('My Name is')
+for i in range(5):
+  print('chika' + str(i))
+print('Slim Shady')
+
+##The for keyword
+##• A variable name
+##• The in keyword 
+##• A call to the range() method with up to three integers passed to it
+##• A colon
+##• Starting on the next line, an indented block of code (called the for clause)
