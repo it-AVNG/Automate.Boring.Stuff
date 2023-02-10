4982ec6ad3a92087c944e7f62192692578bb20b5
it-AVNG <it.avng@gmail.com>
2023-02-10 18:57:02 +0700

    example of slicing practice of a list

---
+++ chapter4_list/sliceing.py
@@ -0,0 +1,11 @@
+spam = ['cat','bat','rat','elephant']
+print('slice from index 0 to 4')
+print(spam[0:4])
+print('slice from index 1 to 3')
+print(spam[1:3])
+print('slice from index 0 to the second to last index')
+print(spam[0:-1])
+print('slice from index 1 to end')
+print(spam[1:])
+print('print all with spam(:)')
+print(spam[:])
