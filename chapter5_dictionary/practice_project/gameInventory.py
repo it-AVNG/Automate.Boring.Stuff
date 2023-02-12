b218b37eea49c90f69a2124866b66ffd15930e10
it-AVNG <it.avng@gmail.com>
2023-02-12 17:37:54 +0700

    change local variable of the function from  to

--- chapter5_dictionary/practice_project/gameInventory.py
+++ chapter5_dictionary/practice_project/gameInventory.py
@@ -18,7 +18,7 @@ inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def displayinventory(chest):
   print("Inventory:")
   item_total = 0
-  for k, v in inventory.items():
+  for k, v in chest.items():
     print(str(v) + ' ' + k)
     item_total += v
   print('Total number of items: ' + str(item_total))
