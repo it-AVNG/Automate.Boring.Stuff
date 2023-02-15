af95ac20a7a4dbcd5296e70cdec531995c4779a0
it-AVNG <it.avng@gmail.com>
2023-02-15 16:53:19 +0700

    basic understanding of REGEX

---
+++ chapter7_REGEX/greedy.py
@@ -0,0 +1,3 @@
+#greedy search is set as default, to be non-greedy, use ? at the end
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+greedyHaRegex = re.compile(r'(Ha){3,5}')
---
+++ chapter7_REGEX/pipe.py
@@ -0,0 +1,3 @@
+import re
+heroRegex = re.compile(r'Bat(man|mobile|copter)') # match any of the word in the pipe
+heroineRegex = re.compile(r'bat(wo)?man')
