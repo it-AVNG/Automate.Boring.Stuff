f7f02c5ff474847169d2f6b5e8c67799821348fc
it-AVNG <it.avng@gmail.com>
2023-02-15 20:09:45 +0700

    example using optional matching for REGEX

--- chapter7_REGEX/pipe.py
+++ chapter7_REGEX/pipe.py
@@ -1,3 +1,4 @@
 import re
 heroRegex = re.compile(r'Bat(man|mobile|copter)') # match any of the word in the pipe
-heroineRegex = re.compile(r'bat(wo)?man')
+heroineRegex = re.compile(r'bat(wo)?man') #optional for (wo)
+
