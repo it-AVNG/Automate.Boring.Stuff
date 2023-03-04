9c6e336a4c99013a72fae2eae3ad30b29a99f4da
it-AVNG <it.avng@gmail.com>
2023-03-05 00:21:26 +0700

    exmple using REGEX for HTPP request

---
+++ PY4E/Networking/curlREGEX.py
@@ -0,0 +1,21 @@
+#One simple way to parse HTML is to use REGEX to
+#Repeatedly search for and extract substrings that mathc
+
+#Search for link values within URL input
+
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#Take input parameters
+url = input('Enter -')
+html = urllib.request.urlopen(url,context = ctx).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
+
